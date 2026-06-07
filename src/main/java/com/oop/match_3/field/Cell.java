eb727a12702bb077db335cdad065e5b138c57535
tihiyn <tihiyn58067@gmail.com>
2026-06-07 21:23:45 +0300

    [Task-11] Added Cell implementation

--- src/main/java/com/oop/match_3/field/Cell.java
+++ src/main/java/com/oop/match_3/field/Cell.java
@@ -1,29 +1,47 @@
 package com.oop.match_3.field;
 
 public class Cell extends CellADT {
+    private final CoordsADT coords;
+    private Element element;
+    private int printStatus;
+
     public Cell(final CoordsADT coords, final Element element) {
         super(coords, element);
+        this.coords = coords;
+        this.element = element;
     }
 
     public boolean hasSameElementAs(final CellADT other) {
-        return false;
+        return element == rawElement(other);
     }
 
-    public boolean isAt(final CoordsADT coords) {
-        return false;
+    public boolean isAt(final CoordsADT target) {
+        return coords.eq(target);
     }
 
     public String print() {
-        return "";
+        printStatus = PRINT_OK;
+        if (element == Element.NONE) {
+            printStatus = PRINT_EMPTY;
+        }
+        return element.name();
     }
 
     public void swap(final CellADT other) {
+        Element mine = element;
+        assignElement(rawElement(other));
+        other.assignElement(mine);
     }
 
-    public void assignElement(final Element element) {
+    public void assignElement(final Element replacement) {
+        element = replacement;
     }
 
     public int getPrintStatus() {
-        return PRINT_EMPTY;
+        return printStatus;
+    }
+
+    private Element rawElement(final CellADT other) {
+        return ((Cell) other).element;
     }
 }
