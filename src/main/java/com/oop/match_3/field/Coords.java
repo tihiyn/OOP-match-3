1ec4716daf5f5119917119a2cbc4eead059de84a
tihiyn <tihiyn58067@gmail.com>
2026-06-07 21:10:33 +0300

    [Task-11] Added Coords implementation

--- src/main/java/com/oop/match_3/field/Coords.java
+++ src/main/java/com/oop/match_3/field/Coords.java
@@ -1,17 +1,38 @@
 package com.oop.match_3.field;
 
+import com.oop.match_3.math.UnsignedInt;
 import com.oop.match_3.math.UnsignedIntADT;
 
 public class Coords extends CoordsADT {
+    private static final UnsignedIntADT ONE = new UnsignedInt(1);
+
+    private final UnsignedIntADT col;
+    private final UnsignedIntADT row;
+
     public Coords(final UnsignedIntADT col, final UnsignedIntADT row) {
         super(col, row);
+        this.col = col;
+        this.row = row;
     }
 
     public boolean isNeighbour(final CoordsADT other) {
-        return false;
+        UnsignedIntADT colDistance = col.subtract(rawCol(other));
+        UnsignedIntADT rowDistance = row.subtract(rawRow(other));
+        UnsignedIntADT distance = colDistance.add(rowDistance);
+        return distance.eq(ONE);
     }
 
     public boolean eq(final CoordsADT other) {
-        return false;
+        boolean sameCol = col.eq(rawCol(other));
+        boolean sameRow = row.eq(rawRow(other));
+        return sameCol && sameRow;
+    }
+
+    private UnsignedIntADT rawCol(final CoordsADT other) {
+        return ((Coords) other).col;
+    }
+
+    private UnsignedIntADT rawRow(final CoordsADT other) {
+        return ((Coords) other).row;
     }
 }
